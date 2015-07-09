f82286194fe5cdab081d46e9ff03111bb4ec2b17
ninagordy <ninahgordy@gmail.com>
2015-07-09 12:08:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+# runs 'code_challenge.py' using the 'tweets.txt' input file and the 'ft1.txt' and 'ft2.txt' output files
 python ./src/code_challenge.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt ./tweet_output/ft2.txt
